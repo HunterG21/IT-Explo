176f8b18da211fcbc1120ead9c2fcb88ef8322ff
Hunter Guenther <guentherh@students.westerntc.edu>
2024-03-28 14:45:51 -0500

    stuff

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ window.addEventListener("load", () => {
     const asideElem = document.querySelector("#aside");
     const btnGlove = document.querySelector("#glove");
     const btnMonitor = document.querySelector("#monitor");
+    const glovePrice = document.querySelector("#glovePrice");
+    const monitorPrice = document.querySelector("#monitorPrice");
     btnGlove.addEventListener("click", () => {
         const imgGlove = document.createElement("img");
         imgGlove.setAttribute("src", "images/glove.jpg");
@@ -9,25 +11,32 @@ window.addEventListener("load", () => {
         removeBtn.setAttribute("type", "Button");
         removeBtn.setAttribute("value", "Remove Item");
         asideElem.append(imgGlove);
+        asideElem.append(glovePrice);
         asideElem.append(removeBtn);
         removeBtn.addEventListener("click", () => {
             asideElem.removeChild(imgGlove);
+            asideElem.removeChild(glovePrice);
             asideElem.removeChild(removeBtn);
-        })
+        }, {once: true})
     })
     btnMonitor.addEventListener("click", () => {
         const imgMonitor = document.createElement("img");
         imgMonitor.setAttribute("src", "images/monitor.jpg");
+        const monitorPrice2 = document.createElement("h4");
+        monitorPrice2.setAttribute("name", monitorPrice);
         const removeBtn = document.createElement("input");
         removeBtn.setAttribute("type", "Button");
         removeBtn.setAttribute("value", "Remove Item");
         asideElem.append(imgMonitor);
+        asideElem.append(monitorPrice2);
         asideElem.append(removeBtn);
         removeBtn.addEventListener("click", () => {
             asideElem.removeChild(imgMonitor);
+            asideElem.removeChild(monitorPrice2);
             asideElem.removeChild(removeBtn);
         })
     })
+
 
 
 })
