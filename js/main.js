f307a77403e609a5d6d8daa6182df23bf965835f
Hunter Guenther <guentherh@students.westerntc.edu>
2024-04-02 13:10:42 -0500

    idk

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ window.addEventListener("load", () => {
     const asideElem = document.querySelector("#aside");
     const btnGolfBall = document.querySelector("#golfBall");
     const btnMonitor = document.querySelector("#monitor");
-<<<<<<< HEAD
+
     const totalPrice = document.querySelector("#tPrice")
 
     var addItemButton = document.getElementById("addItemButton");
@@ -27,43 +27,8 @@ window.addEventListener("load", () => {
             asideElem.removeChild(imgGolfBall);
             asideElem.removeChild(removeItemButton);
             
-=======
-    const glovePrice = document.querySelector("#glovePrice");
-    const monitorPrice = document.querySelector("#monitorPrice");
-    btnGlove.addEventListener("click", () => {
-        const imgGlove = document.createElement("img");
-        imgGlove.setAttribute("src", "images/glove.jpg");
-        const removeBtn = document.createElement("input");
-        removeBtn.setAttribute("type", "Button");
-        removeBtn.setAttribute("value", "Remove Item");
-        asideElem.append(imgGlove);
-        asideElem.append(glovePrice);
-        asideElem.append(removeBtn);
-        removeBtn.addEventListener("click", () => {
-            asideElem.removeChild(imgGlove);
-            asideElem.removeChild(glovePrice);
-            asideElem.removeChild(removeBtn);
-        }, {once: true})
-    })
-    btnMonitor.addEventListener("click", () => {
-        const imgMonitor = document.createElement("img");
-        imgMonitor.setAttribute("src", "images/monitor.jpg");
-        const monitorPrice2 = document.createElement("h4");
-        monitorPrice2.setAttribute("name", monitorPrice);
-        const removeBtn = document.createElement("input");
-        removeBtn.setAttribute("type", "Button");
-        removeBtn.setAttribute("value", "Remove Item");
-        asideElem.append(imgMonitor);
-        asideElem.append(monitorPrice2);
-        asideElem.append(removeBtn);
-        removeBtn.addEventListener("click", () => {
-            asideElem.removeChild(imgMonitor);
-            asideElem.removeChild(monitorPrice2);
-            asideElem.removeChild(removeBtn);
->>>>>>> 176f8b18da211fcbc1120ead9c2fcb88ef8322ff
         })
     })
-    
 
     
 
