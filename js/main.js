475cf50772bd7905665c57eecd7481ffc1b4be75
Hunter Guenther <guentherh@students.westerntc.edu>
2024-04-02 10:07:16 -0500

    in class work

--- js/main.js
+++ js/main.js
@@ -1,33 +1,38 @@
 window.addEventListener("load", () => {
     const asideElem = document.querySelector("#aside");
-    const btnGlove = document.querySelector("#glove");
+    const btnGolfBall = document.querySelector("#golfBall");
     const btnMonitor = document.querySelector("#monitor");
-    btnGlove.addEventListener("click", () => {
-        const imgGlove = document.createElement("img");
-        imgGlove.setAttribute("src", "images/glove.jpg");
-        const removeBtn = document.createElement("input");
-        removeBtn.setAttribute("type", "Button");
-        removeBtn.setAttribute("value", "Remove Item");
-        asideElem.append(imgGlove);
-        asideElem.append(removeBtn);
-        removeBtn.addEventListener("click", () => {
-            asideElem.removeChild(imgGlove);
-            asideElem.removeChild(removeBtn);
+    const totalPrice = document.querySelector("#tPrice")
+
+    var addItemButton = document.getElementById("addItemButton");
+    // var removeItemButton = document.getElementById("removeItemButton");
+    var totalCostElement = document.getElementById("totalCost");
+    var totalCost = 0;
+
+    
+
+    btnGolfBall.addEventListener("click", () => {
+        const imgGolfBall = document.createElement("img");
+        imgGolfBall.setAttribute("src", "images/GolfBalls.png");
+        imgGolfBall.setAttribute("style", "width: 70%;");
+        const removeItemButton = document.createElement("input");
+        removeItemButton.setAttribute("type", "Button");
+        removeItemButton.setAttribute("value", "Remove Item");
+        asideElem.append(imgGolfBall);
+        asideElem.append(removeItemButton);
+        asideElem.insertBefore(imgGolfBall, totalCostElement);
+        asideElem.insertBefore(removeItemButton, totalCostElement);
+        removeItemButton.addEventListener("click", () => {
+            asideElem.removeChild(imgGolfBall);
+            asideElem.removeChild(removeItemButton);
+            
         })
     })
-    btnMonitor.addEventListener("click", () => {
-        const imgMonitor = document.createElement("img");
-        imgMonitor.setAttribute("src", "images/monitor.jpg");
-        const removeBtn = document.createElement("input");
-        removeBtn.setAttribute("type", "Button");
-        removeBtn.setAttribute("value", "Remove Item");
-        asideElem.append(imgMonitor);
-        asideElem.append(removeBtn);
-        removeBtn.addEventListener("click", () => {
-            asideElem.removeChild(imgMonitor);
-            asideElem.removeChild(removeBtn);
-        })
-    })
+    
+
+    
+
+
 
 
 })
